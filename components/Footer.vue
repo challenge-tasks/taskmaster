7aa9ebd3f5e1c6890dcd490850f9a93d1be635ee
Aslbek Kucharov <aslbek@bulavka.uz>
2023-08-09 14:16:02 +0500

    feat: add social links for community

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,17 @@
 <template>
-    <footer></footer>
+    <footer class="footer">
+        <div class="mx-auto tm-container">
+
+            <div class="socials">
+                <a href="https://t.me/+sNukVGsJnzFjYzcy" class="socials-item socials-item--telegram">
+                    <Icon name="akar-icons:telegram-fill" class="socials-item__icon" />
+                </a>
+                <a href="https://discord.gg/hQXccYgm" class="socials-item socials-item--discord">
+                    <Icon name="jam:discord" class="socials-item__icon" />
+                </a>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script setup>
--- components/Header.vue
+++ components/Header.vue
@@ -6,13 +6,13 @@
                 <img src="../assets/icons/logo.svg" alt="Logotype">
             </NuxtLink>
 
-            <span class="burger">
-                <span class="burger-sausage"></span>
-                <span class="burger-sausage"></span>
-                <span class="burger-sausage"></span>
+            <span @click="toggleMenu" class="burger" :class="{ 'burger--active': isMenuActive }">
+                <span class="burger__sausage"></span>
+                <span class="burger__sausage"></span>
+                <span class="burger__sausage"></span>
             </span>
 
-            <div class="menu">
+            <div class="menu" :class="{ 'menu--active': isMenuActive }">
                 <NuxtLink to="/tasks" class="menu__item">Задания</NuxtLink>
                 <NuxtLink to="/about" class="menu__item">О проекте</NuxtLink>
                 <Button @click="toggleSignInModal" label="Войти" class="btn--primary btn--rounded menu__btn" :icon="{ name: 'octicon:person-24', class: 'btn__icon--right' }" />
@@ -23,6 +23,18 @@
 </template>
 
 <script setup lang="ts">
+
+const isMenuActive = ref(false)
+
+function toggleMenu() {
+    isMenuActive.value = !isMenuActive.value
+
+    useHead({
+        bodyAttrs: {
+            class: isMenuActive.value ? 'overflow-hidden' : ''
+        }
+    })
+}
 
 const { toggleSignInModal } = useAuthentication()
 
