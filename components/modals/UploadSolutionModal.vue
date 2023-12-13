633c72f5ff19dafcaa6b9098c5fd81642c75f914
Aslbek Kucharov <aslbek@bulavka.uz>
2023-12-13 18:15:37 +0500

    feat: upload solution via github link

--- components/modals/UploadSolutionModal.vue
+++ components/modals/UploadSolutionModal.vue
@@ -3,22 +3,47 @@
 
         <div class="p-4">
 
-            <div class="mb-4 p-3 transition linear delay-150 w-full h-56 flex items-center justify-center relative rounded-md border-2 hover:border-gray-700 border-gray-400 border-dashed">
-                <div class="flex flex-col gap-2 items-center justify-center">
-                    <ClientOnly>
-                        <span class="flex items-center mb-2">
-                            <UIcon name="i-ant-design-cloud-upload-outlined" class="text-slate-400 w-10 h-10" />
-                        </span>
-                    </ClientOnly>
-                    <span class="max-w-md text-center text-slate-400">Выберите .zip или .rar файл где содержится решение по заданию и загрузите его для проверки</span>
-                </div>
-                <input type="file" accept=".zip,.rar,.7z,.gz" maxlength="1" ref="fileInput" @change="handleFile" class="opacity-0 absolute top-0 left-0 right-0 bottom-0">
-            </div>
+            <UTabs :items="tabs">
 
-            <div v-if="form.file" class="flex justify-between gap-2 mb-4 bg-slate-50 py-3 px-2 rounded-md">
-                <span class="mr-auto text-slate-600">{{ form.file.name }}</span>
-                <span class="ml-auto text-slate-600">{{ fileSize }}</span>
-            </div>
+                <template v-slot:file_upload>
+
+                    <div class="my-4 p-3 transition linear delay-150 w-full h-56 flex items-center justify-center relative rounded-md border-2 hover:border-gray-700 border-gray-400 border-dashed">
+                        <div class="flex flex-col gap-2 items-center justify-center">
+                            <ClientOnly>
+                                <span class="flex items-center mb-2">
+                                    <UIcon name="i-ant-design-cloud-upload-outlined" class="text-slate-400 w-10 h-10" />
+                                </span>
+                            </ClientOnly>
+                            <span class="max-w-md text-center text-slate-400">Выберите .zip или .rar файл где содержится решение по заданию и загрузите его для проверки</span>
+                            </div>
+                            <input type="file" accept=".zip,.rar,.7z,.gz" maxlength="1" ref="fileInput" @change="handleFile" class="opacity-0 absolute top-0 left-0 right-0 bottom-0">
+                        </div>
+
+                        <div v-if="form.file" class="flex justify-between gap-2 mb-4 bg-slate-50 py-3 px-2 rounded-md">
+                            <span class="mr-auto text-slate-600">{{ form.file.name }}</span>
+                            <span class="ml-auto text-slate-600">{{ fileSize }}</span>
+                        </div>
+
+                        <!-- <div class="w-full flex flex-col xs:flex-row justify-end gap-2">
+                            <UButton @click="cancelUpload" variant="soft" class="w-full xs:w-auto py-3 btn order-1 xs:order-0">
+                                Отменить
+                            </UButton>
+                            <UButton @click="uploadFileToServer" trailing :disabled="!form.file" :loading="isTaskSolutionUploading" class="order-0 xs:order-1 w-full xs:w-auto py-3 btn btn--primary">
+                                {{ uploadButtonLabel }}
+                            </UButton>
+                        </div> -->
+                </template>
+
+                <template v-slot:github_url>
+                    <Form class="my-4">
+                        <UInput placeholder="Вставьте ссылку на Github репозиторий" />
+                        <!-- <UButton @click="uploadFileToServer" trailing :disabled="!form.file" :loading="isTaskSolutionUploading" class="order-0 xs:order-1 w-full xs:w-auto py-3 btn btn--primary">
+                            {{ uploadButtonLabel }}
+                        </UButton> -->
+                    </Form>
+                </template>
+
+            </UTabs>
 
             <div class="w-full flex flex-col xs:flex-row justify-end gap-2">
                 <UButton @click="cancelUpload" variant="soft" class="w-full xs:w-auto py-3 btn order-1 xs:order-0">
@@ -53,6 +78,17 @@ const emit = defineEmits<{
     (e: 'upload-success'): void 
     (e: 'update:modelValue', id: boolean): void 
 }>()
+
+const tabs: Array<{ label: string, slot: string }> = [
+    {
+        slot: 'file_upload',
+        label: 'Отправить файл'
+    },
+    {
+        slot: 'github_url',
+        label: 'Отправить ссылку на Github'
+    }
+]
 
 const fileInput = ref<HTMLFormElement>()
 
